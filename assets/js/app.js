1e36ee7b204f1e0149cfc749fc87aa75fc5d7f54
priyadarshni patil <priyadarshni0973p@gmail.com>
2025-02-04 11:01:31 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,20 +24,20 @@ let bars1 = document.querySelectorAll('.hamburger1 span')
 let isActive = false
 let isActive1 = false
 
-hamburger.addEventListener('click', function() {
-    mobileNav.classList.toggle('open')
-    if(!isActive) {
-        bars[0].style.transform = 'rotate(45deg)'
-        bars[1].style.opacity = '0'
-        bars[2].style.transform = 'rotate(-45deg)'
-        isActive = true
-    } else {
-        bars[0].style.transform = 'rotate(0deg)'
-        bars[1].style.opacity = '1'
-        bars[2].style.transform = 'rotate(0deg)'
-        isActive = false
-    }
-})
+// hamburger.addEventListener('click', function() {
+//     mobileNav.classList.toggle('open')
+//     if(!isActive) {
+//         bars[0].style.transform = 'rotate(45deg)'
+//         bars[1].style.opacity = '0'
+//         bars[2].style.transform = 'rotate(-45deg)'
+//         isActive = true
+//     } else {
+//         bars[0].style.transform = 'rotate(0deg)'
+//         bars[1].style.opacity = '1'
+//         bars[2].style.transform = 'rotate(0deg)'
+//         isActive = false
+//     }
+// })
 
 hamburger1.addEventListener('click', function() {
     mobileNav.classList.toggle('open')
